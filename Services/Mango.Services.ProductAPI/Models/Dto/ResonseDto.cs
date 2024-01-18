5b06a12bddc022ed87a6f03fc6d7244cd1d4d84c
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-01-18 10:55:43 +0530

    ProductDto

---
+++ Services/Mango.Services.ProductAPI/Models/Dto/ResonseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ProductAPI.Models.Dto
+{
+    public class ResonseDto
+    {
+
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; }
+        public List<string> ErrorMessage { get; set; }
+
+    }
+}

79bcb5ba039508a51c812c0ce5740155e1bfa449
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-01-18 13:38:15 +0530

    Product API Controller HTTPGET

---
+++ Services/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private readonly IProductRepository _productRepository;
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+            this._response= new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result=productDtos;
+
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess= false;
+                _response.ErrorMessage=new List<string>() { ex.ToString()};
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetPRoductById(id);
+                _response.Result = productDto;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}

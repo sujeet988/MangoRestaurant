0d2d5085a6e94af7648e512748ffd857bc68699e
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-01-20 19:56:47 +0530

    Push model to cart database

--- Services/Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
+++ Services/Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.DbContexts
 {
@@ -8,6 +9,9 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
         {
 
         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
 
     }
 }

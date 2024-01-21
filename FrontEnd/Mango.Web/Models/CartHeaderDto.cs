ceee0aabb706618459857d5e1765d8f279735391
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-01-21 18:28:04 +0530

    checkout functionality

--- FrontEnd/Mango.Web/Models/CartHeaderDto.cs
+++ FrontEnd/Mango.Web/Models/CartHeaderDto.cs
@@ -8,5 +8,15 @@
         public double OrderTotal { get; set; }
 
         public double DiscountTotal { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime PickupDateTime { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string ExpiryMonthYear { get; set; }
+
     }
 }

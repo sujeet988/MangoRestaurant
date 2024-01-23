94e7cc6ca45abb487de14dad30579c565ceadefa
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-01-23 12:39:42 +0530

    Ocelot setup for prodcu api

--- GateWay/Mango.GatewaySolution/Program.cs
+++ GateWay/Mango.GatewaySolution/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
 namespace Mango.GatewaySolution
 {
     public class Program
@@ -5,10 +10,25 @@ namespace Mango.GatewaySolution
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication("Bearer")
+              .AddJwtBearer("Bearer", options =>
+              {
+
+                  options.Authority = "https://localhost:44365/";
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateAudience = false
+                  };
+
+              });
+
+            builder.Services.AddOcelot();
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
-
+            app.UseOcelot();
             app.Run();
         }
     }
